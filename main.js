8711ea1e2c75e88cfb929aac43d0259d796321e1
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-08-07 07:43:58 -0300

    qlqr coisa

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+const palpites = document.querySelctor('.palpites');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
+const envioPalpite = document.querySelector('.envioPalpite');
+const envioPalpite = document.querySelector('.campoPalpite');
+
+
+
+function verificarPalpite() {}
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpites === 1) {
+      palpites.textContent = "Palpites anteriores: ";
+    }
+
+    palpiteUsuario.textContent += palpiteUsuario + " ";
+
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = "Parebens! Você Acertou!";
+    
+    }
